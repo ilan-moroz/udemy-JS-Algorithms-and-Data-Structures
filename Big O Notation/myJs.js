2a48679590612dd7ebd6b6c61a400a5f10cd5319
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-03 18:38:38 +0300

    first

---
+++ Big O Notation/myJs.js
@@ -0,0 +1,83 @@
+//! ** TIME COMPLEXITY **
+// slower ---- O(n)
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+var t1 = performance.now();
+addUpTo(1000000000);
+var t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+
+// faster ---- O(1)
+function addUpTo2(n) {
+  return (n * (n + 1)) / 2;
+}
+
+var time1 = performance.now();
+addUpTo2(1000000000);
+var time2 = performance.now();
+console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`);
+
+// nested loops ---- O(n*2)
+const printAllPairs = n => {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      console.log(i, j);
+    }
+  }
+};
+
+// max 4 loops ---- O(1)
+const logAtMost5 = n => {
+  for (let i = 1; i < Math.min(5, n); i++) {
+    console.log(i);
+  }
+};
+
+//* OBJECTS
+let instructor = {
+  firstName: 'John',
+  isInstructor: true,
+  favoriteNumbers: [1, 2, 3],
+};
+// insertion ---- O(1)
+// removal ---- O(1)
+// access ---- O(1)
+// searching ---- O(n)
+// Object.keys ---- O(n)
+// Object.values ---- O(n)
+// Object.entries ---- O(n)
+// hasOwnProperty ---- O(1)
+
+//* ARRAYS
+// access ---- O(1)
+// searching ---- O(n)
+// push,pop ---- O(1)
+// unshift,shift ---- O(n)
+// concat,slice,splice ---- O(n)
+// sort ---- O(n*log n)
+// forEach,map,filter,reducer,etc.. ---- O(n)
+
+//! ** SPACE COMPLEXITY **
+// O(1)
+const sum = arr => {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+};
+
+// (O)n
+const double = arr => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(2 * arr[i]);
+  }
+  return newArr();
+};
---
+++ Problem Solving/myJs.js
@@ -0,0 +1,35 @@
+//! Problem solving
+// * understand the problem
+// * explore concrete examples
+// * break it down
+// * solve/simplify
+
+//! Understand the problem
+//* 1.Can i restate the problem in my own words
+//* 2.what are the inputs that go into the problem
+//* 3.what are the outputs that should come from the solution to the problem
+//* 4.can the outputs be determined from the inputs(do i have enough info to solve the problem)?
+//* 5.how to should i label the important pieces of data that are a part the problem
+
+const countChar = str => {
+  const count = {};
+  for (let i = 0; i < str.length; i++) {
+    const lowerCase = str[i].toLowerCase();
+    if (lowerCase !== ' ')
+      if (count[lowerCase]) count[lowerCase] += 1;
+      else count[lowerCase] = 1;
+  }
+  return count;
+};
+
+console.log(countChar('my phone number is 2222'));
+
+const countChar2 = str => {
+  return str.split('').reduce((acc, char) => {
+    const lowerCase = char.toLowerCase();
+    if (lowerCase !== ' ') acc[lowerCase] = (acc[lowerCase] || 0) + 1;
+    return acc;
+  }, {});
+};
+
+console.log(countChar2('a    baAAAAb'));
