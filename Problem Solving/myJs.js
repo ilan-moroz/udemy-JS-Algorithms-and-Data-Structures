01cbae4f638669f752e268a531898c42b03905af
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-05 14:48:40 +0300

    maxSubarraySum2

--- Problem Solving/myJs.js
+++ Problem Solving/myJs.js
@@ -178,3 +178,22 @@ function maxSubarraySum(arr, num) {
 
 console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2));
 console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4));
+
+// O(n)
+function maxSubarraySum2(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+console.log(maxSubarraySum2([1, 2, 5, 2, 8, 1, 5], 2));
+console.log(maxSubarraySum2([1, 2, 5, 2, 8, 1, 5], 4));
